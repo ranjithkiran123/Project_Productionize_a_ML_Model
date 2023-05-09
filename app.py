4b5b64bd2f93005d2a65492d8adb3ddfdb104152
ranjithkiran123 <92369725+ranjithkiran123@users.noreply.github.com>
2023-05-09 21:40:09 +0530

    app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if st.button('Predict'):
     prediction = lr.predict(query_point_transformed)
     if prediction == 0:
         st.success("You don't have Diabetes 😊!")
-        st.image("C:\Users\A.Ranjith Kiran\DATA\images.jpeg")
+        st.image(images.jpeg)
     else:
         st.error("You have Diabetes 😥!")
-        st.image("C:\Users\A.Ranjith Kiran\DATA\sad diabetes.jpg")
+        st.image(sad diabetes.jpg)
